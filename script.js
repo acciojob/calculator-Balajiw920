050f8cf3bfd0a97e80a62cd934859aa9743c5bf2
Balajiw920 <wbalaji2021@gmail.com>
2023-01-08 21:10:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let input = document.getElementById("input");
+let buttons = document.getElementsByTagName("button");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    let btnVal
