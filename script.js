6f5300a3e94f5a42c63c0db75931df903c061ade
Balajiw920 <wbalaji2021@gmail.com>
2023-01-08 21:14:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-//your code here
 let input = document.getElementById("input");
 let buttons = document.getElementsByTagName("button");
 
+let firstVal = "";
+let secondVal = "";
+let result = "";
+let operator = "";
+
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function() {
-    let btnVal
+    let btnVal = this.innerHTML;
+
+    if (btnVal === "AC") {
+      first
