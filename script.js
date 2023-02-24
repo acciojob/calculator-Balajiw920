1b7df7e98b92a108fd22c5796f4d39e2c6793f72
Balajiw920 <wbalaji2021@gmail.com>
2023-02-24 21:23:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-let input = document.getElementById("input");
-let buttons = document.getElementsByTagName("button");
 
-let firstVal = "";
-let secondVal = "";
-let result = "";
-let operator = "";
-
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function() {
-    let btnVal = this.innerHTML;
-
-    if (btnVal === "AC") {
-      first
